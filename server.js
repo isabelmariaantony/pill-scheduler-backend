acf6efad5633bc961a5f375ff6ec4e5c27eeda0e
Sheeba Ann George <shgeorge@Sheebas-MacBook-Pro.local>
2024-05-01 13:35:39 -0700

    fixed the time issue

--- server.js
+++ server.js
@@ -29,10 +29,9 @@ function getCurrentHourInPST() {
     return parseInt(hourString, 10); // Converts the hour string to an integer
 }
 
-console.log(getCurrentHourInPST()); // Outputs the current hour in PST or PDT as an integer
 
 function getCurrentTimeRange() {
-    const hour = getCurrentHourInPST;
+    const hour = getCurrentHourInPST();
     if (hour >= 5 && hour < 10) return 'morning';
     if (hour >= 10 && hour < 12) return 'noon';
     if (hour >= 12 && hour < 16) return 'afternoon';
